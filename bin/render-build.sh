bd6cd0e12d387ca72c490229a43d960e6a553448
dochin26 <yuuta.2635@gmail.com>
2025-05-13 11:01:53 +0900

    fix:db settings unsafe

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,6 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # bundle exec rake db:prepare
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:force_drop
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop:_unsafe
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 bundle exec rails db:seed
