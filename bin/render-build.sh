1fd531e4edc716863fea295ebbc6d966f4bc64ce
dochin26 <yuuta.2635@gmail.com>
2025-05-13 19:34:30 +0900

    fix: render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:prepare
+bundle exec rake db:prepare
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 bundle exec rails db:seed
